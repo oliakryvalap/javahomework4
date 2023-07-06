755559d1f32912780fb96a0fd91996de6579cc6e
Olga <olia_tarasenko@inbox.ru>
2023-07-06 03:57:17 +0300

    first commit

---
+++ task1.java
@@ -0,0 +1,25 @@
+// пусть дан LinkedList с несколькими элементами. 
+// реализуйте метод, который вернет " перевернутый список." 
+
+import java.util.Arrays;
+import java.util.LinkedList;
+
+public class task1 {
+    public static void main(String[] args) {
+        LinkedList<Integer> ls = new LinkedList<>(Arrays.asList(1,2,3,4,5));
+        System.out.println(reverse(ls));
+    }
+    
+    public static LinkedList<Integer> reverse(LinkedList<Integer> ls) {
+        LinkedList<Integer> rev_ls = new LinkedList<>();
+        for(int i=ls.size()-1;i>=0;i--){
+            rev_ls.add(ls.get(i));
+        }
+        return rev_ls;
+
+        
+    } 
+
+        
+}
+    
